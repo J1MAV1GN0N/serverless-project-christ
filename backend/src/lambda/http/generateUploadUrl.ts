38fc5086acdad23c197761366be4df7c96b04bcb
Philipp Christ <philipp.christ.1997@gmail.com>
2021-03-05 16:07:39 +0100

    chore: Add necessary files
    Added create, delete, get and update files for todo application

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,22 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
+
+import { generateUploadUrl } from '../../businessLogic/todos';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-
+  
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const signedUrl = await generateUploadUrl(event);
+  console.log('signed url ', signedUrl)
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: signedUrl
+    })
+  };
 }
