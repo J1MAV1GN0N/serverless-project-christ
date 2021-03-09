d99a63df09a2c50523f1139eccd921ab9c06a636
Philipp Christ <philipp.christ.1997@gmail.com>
2021-03-09 15:26:34 +0100

    chore: Update utils

--- backend/src/lambda/utils.ts
+++ backend/src/lambda/utils.ts
@@ -8,7 +8,7 @@ import { parseUserId } from "../auth/utils";
  * @returns a user id from a JWT token
  */
  export function getUserId(event: APIGatewayProxyEvent): string {
-  const authorization = event.headers.authorization  //const authorization = event.headers.Authorization 
+  const authorization = event.headers.Authorization
 // There is no Authorization 
 // property but a authorization property
   console.log("Value of Authorization:- " + authorization)
